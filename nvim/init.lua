690ccef36e0d04b5dc5234c799a1a5c118ad3449
Alexander Joukov <ajoukov@cs.stonybrook.edu>
2025-01-29 02:53:35 -0500

    init

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+require("ajoukov")
+
+-- Show line numbers and relative numbers
+vim.o.relativenumber = true
+vim.o.number = true
+
+-- Use 4 spaces instead of tabs
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- Highlight search matches while searching
+vim.o.hlsearch = true
+vim.o.incsearch = true
+
+-- Enable intelligent indentation
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.smarttab = true
+
+
