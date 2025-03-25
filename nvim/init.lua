6bedf46d3829db4fba95dde4975d7bccad0a7b4b
Ajoukov <aapteryx@gmail.com>
2025-03-25 19:06:31 -0400

    <leader> v opens :Ex

--- nvim/init.lua
+++ nvim/init.lua
@@ -134,3 +134,34 @@ vim.diagnostic.config({
 })
 vim.opt.signcolumn = "no"
 
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = false,
+  underline = false,
+  update_in_insert = false,
+})
+vim.opt.signcolumn = "no"
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>s', function()
+        builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
+
+
+vim.keymap.set("n", "<leader>v", vim.cmd.Ex)
+
