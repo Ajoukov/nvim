7bebe2dfce93d9304fd8473b2a4e1da3ea6a3b76
Ajoukov <aapteryx@gmail.com>
2025-03-25 19:01:11 -0400

    Just do packersync

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,136 @@
-require("ajoukov")
+-- Bootstrap Packer
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({
+      "git",
+      "clone",
+      "--depth",
+      "1",
+      "https://github.com/wbthomason/packer.nvim",
+      install_path,
+    })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
+ensure_packer()
 
--- Show line numbers and relative numbers
-vim.o.relativenumber = true
-vim.o.number = true
+-- Auto-sync on save
+vim.cmd([[ 
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost init.lua source <afile> | PackerSync
+  augroup end
+]])
 
--- Use 4 spaces instead of tabs
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = true
+-- Plugins
+require("packer").startup(function(use)
+  use "wbthomason/packer.nvim"
 
--- Highlight search matches while searching
-vim.o.hlsearch = true
-vim.o.incsearch = true
+  -- Telescope
+  use {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    requires = { "nvim-lua/plenary.nvim" }
+  }
 
--- Enable intelligent indentation
-vim.o.autoindent = true
-vim.o.smartindent = true
-vim.o.smarttab = true
+  -- Harpoon
+  use "ThePrimeagen/harpoon"
 
-vim.opt.scrolloff = 8
+  -- LSP
+  use "neovim/nvim-lspconfig"
+  use { "williamboman/mason.nvim", run = ":MasonUpdate" }
+  use "williamboman/mason-lspconfig.nvim"
+
+  -- Autocompletion
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "L3MON4D3/LuaSnip"
+  use "saadparwaiz1/cmp_luasnip"
+  use "hrsh7th/cmp-buffer"
+  use "hrsh7th/cmp-path"
+end)
+
+-- Telescope keymaps
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+
+-- Harpoon keymaps
+local harpoon = require("harpoon")
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+
+-- LSP Setup
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "clangd",
+  },
+  automatic_installation = true,
+})
+
+local lspconfig = require("lspconfig")
+require("mason-lspconfig").setup_handlers({
+  function(server_name)
+    lspconfig[server_name].setup({})
+  end,
+})
+
+-- Lua-specific LSP tweaks
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" }
+      }
+    }
+  }
+})
+
+-- LSP Keymaps (on_attach)
+local lsp = require("lspconfig")
+
+lsp.on_attach = function(_, bufnr)
+  local opts = { buffer = bufnr, remap = false }
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+end
+
+-- Autocompletion setup
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+  mapping = {
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  }
+})
+
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = false,
+  underline = false,
+  update_in_insert = false,
+})
+vim.opt.signcolumn = "no"
 
