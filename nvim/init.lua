46b53a50db3d002b425b2b4a345efa0266572287
Ajoukov <aapteryx@gmail.com>
2025-08-26 00:38:06 -0400

    single-file setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,10 @@
+-- Single-file Neovim setup
+-- Keep all configuration here to avoid scattered files.
+
+local nerd_font = "JetBrainsMono Nerd Font Mono"
+vim.opt.guifont = nerd_font .. ":h14"     -- pick whatever size you like
+
+
 -- Bootstrap Packer
 local ensure_packer = function()
   local fn = vim.fn
@@ -26,9 +33,49 @@ vim.cmd([[
   augroup end
 ]])
 
+-- pick up your shell’s OPENAI_API_KEY in Neovim’s env (no secrets in repo)
+-- vim.env.OPENAI_API_KEY = vim.fn.getenv("OPENAI_API_KEY")
+
 -- Plugins
 require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
+  
+
+--  -- Avante.nvim (AI-driven code suggestions)
+--  use {
+--      "yetone/avante.nvim",
+--      branch = "main",
+--      requires = {
+--          -- required
+--          "nvim-treesitter/nvim-treesitter",
+--          "stevearc/dressing.nvim",
+--          "nvim-lua/plenary.nvim",
+--          "MunifTanjim/nui.nvim",
+--          {
+--              "MeanderingProgrammer/render-markdown.nvim",
+--              opts = { file_types = { "markdown", "Avante" } },
+--              ft   = { "markdown", "Avante" },
+--          },
+--          -- optional
+--          "hrsh7th/nvim-cmp",
+--          "nvim-tree/nvim-web-devicons",
+--          "HakonHarnes/img-clip.nvim",
+--          "zbirenbaum/copilot.lua",
+--      },
+--      run = "make",
+--      config = function()
+--          require("avante").setup({
+--              provider = "openai",
+--              openai = {
+--                  endpoint    = "https://api.openai.com/v1",
+--                  model       = "gpt-4o",
+--                  timeout     = 60000,
+--                  temperature =   0,
+--                  max_tokens  = 4096,
+--              },
+--          })
+--      end,
+--  }
 
   -- Telescope
   use {
@@ -37,7 +84,7 @@ require("packer").startup(function(use)
     requires = { "nvim-lua/plenary.nvim" }
   }
 
-  -- Harpoon
+  -- Harpoon (v1)
   use "ThePrimeagen/harpoon"
 
   -- LSP
@@ -57,20 +104,15 @@ require("packer").startup(function(use)
 
 end)
 
+-- Leader and basic mappings
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>v", vim.cmd.Ex)
+
 -- Telescope keymaps
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-
--- Harpoon keymaps
-local harpoon = require("harpoon")
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
 -- LSP Setup
 require("mason").setup()
@@ -155,36 +197,15 @@ vim.diagnostic.config({
 })
 vim.opt.signcolumn = "no"
 
-vim.diagnostic.config({
-  virtual_text = false,
-  signs = false,
-  underline = false,
-  update_in_insert = false,
-})
-vim.opt.signcolumn = "no"
-
+-- Harpoon keymaps (v1)
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
-
-vim.g.mapleader = " "
-
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>s', function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-
-
-vim.keymap.set("n", "<leader>v", vim.cmd.Ex)
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -208,10 +229,6 @@ vim.g.vimtex_compiler_latexmk = {
   },
 }
 
-vim.g.vimtex_mappings_enabled = 0
--- Set leader to space (if not already set)
-vim.g.mapleader = " "
-
 -- Disable default vimtex mappings
 vim.g.vimtex_mappings_enabled = 0
 
@@ -227,3 +244,56 @@ vim.api.nvim_set_keymap('v', '*', '"+y:let @* = getreg("+")<CR>', { noremap = tr
 vim.api.nvim_set_keymap('n', '*', '"+y:let @* = getreg("+")<CR>', { noremap = true, silent = true })
 
 
+local function log_var()
+  -- yank visual selection into "z
+  vim.cmd('silent! normal! "zy')
+
+  -- get the text
+  local var = vim.fn.getreg('z')
+  if var == '' then
+    vim.notify('No selection to log', vim.log.levels.WARN)
+    return
+  end
+
+  -- choose template by filetype
+  local ft = vim.bo.filetype
+  local stmt
+  if ft == 'c' then
+    stmt = string.format('printf("%s: %%s\\n", %s);', var, var)
+  elseif ft:match('^js') or ft:match('^javascript') or ft:match('^typescript') then
+    stmt = string.format('console.log("%s: " + JSON.stringify(%s));', var, var)
+  else
+    vim.notify('LogVar: unsupported filetype ' .. ft, vim.log.levels.WARN)
+    return
+  end
+
+  -- append the statement below the current line
+  local row = vim.api.nvim_win_get_cursor(0)[1]
+  vim.api.nvim_buf_set_lines(0, row, row, false, { stmt })
+end
+
+-- map <leader>l in visual mode
+vim.keymap.set('v', '<leader>l', log_var, { silent = true, desc = 'Log variable/value' })
+
+-- make it so that pressing 'o' or 'O' from a comment doesn't insert comment text like "//"
+local no_comment_o = vim.api.nvim_create_augroup("NoCommentO", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = no_comment_o,
+  pattern = "*",
+  callback = function()
+    -- remove 'o' (and optional 'r') from the local formatoptions
+    vim.bo.formatoptions = vim.bo.formatoptions:gsub("o", "")
+    vim.bo.formatoptions = vim.bo.formatoptions:gsub("r", "")
+  end,
+})
+
+vim.keymap.set('n','S',function()
+  local c = vim.fn.nr2char(vim.fn.getchar())
+  vim.api.nvim_feedkeys('i'..c..vim.api.nvim_replace_termcodes('<Esc>','',true,true),'n',false)
+end,{noremap=true})
+
+vim.keymap.set('n','s',function()
+  local c = vim.fn.nr2char(vim.fn.getchar())
+  vim.api.nvim_feedkeys('a'..c..vim.api.nvim_replace_termcodes('<Esc>','',true,true),'n',false)
+end,{noremap=true})
--- nvim/lua/ajoukov/init.lua
+++
@@ -1,2 +0,0 @@
-require("ajoukov.remap")
-
